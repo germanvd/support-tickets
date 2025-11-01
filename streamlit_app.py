8164f248e8e0d07faec62d4c59970eb54b7d4f14
German Dominguez <germanvd@gmail.com>
2025-11-01 00:08:23 -0400

    Update app title to Spanish

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 # Show app title and description.
 st.set_page_config(page_title="Support tickets", page_icon="🎫")
-st.title("🎫 Support tickets")
+st.title("🎫 SOPORTE PARA TIKET")
 st.write(
     """
     This app shows how you can build an internal tool in Streamlit. Here, we are 
